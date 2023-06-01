1af0941a1c2cdfd6db2496b1343460768b8f6e43
pchelaAlina <khannanova.alina2013@icloud.com>
2023-06-01 22:42:55 +0500

    Commit 1

---
+++ tasks/TaskWork2.java
@@ -0,0 +1,10 @@
+public class TaskWork2 {
+    public static void main(String args[]){
+        double x;
+        byte y;
+        x=10.5;
+        y=127;
+        System.out.println("Переменная типа с плавающей точкой с наибольшим диапазоном чисел:"+""+x);
+        System.out.println("Переменная целочисленного типа с наименьшим диапазоном чисел:"+""+y);
+    }
+}
